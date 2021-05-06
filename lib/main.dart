33470ad45324efb70d2010a605ad7c4368682ba2
Riley <rileyqx@yahoo.com.sg>
2021-05-06 21:11:18 +0800

    added template & functions for home/login, functions dont work yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,367 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gather_app/constants.dart';
+import 'package:gather_app/model/user.dart';
+import 'package:gather_app/services/authenticate.dart';
+import 'package:gather_app/services/helper.dart';
+import 'package:gather_app/ui/auth/authScreen.dart';
+import 'package:gather_app/ui/home/homeScreen.dart';
+import 'package:gather_app/ui/login/loginScreen.dart';
+import 'package:gather_app/ui/onBoarding/onBoardingScreen.dart';
+import 'package:gather_app/ui/signUp/signUpScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  MyAppState createState() => MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  static User currentUser;
+  StreamSubscription tokenStream;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+        print("is initialized");
+        print(_initialized);
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushasdny times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    // Show error message if initialization failed
+    if (_error) {
+      return MaterialApp(
+          home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+              child: Column(
+            children: [
+              Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 25,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Failed to initialise firebase!',
+                style: TextStyle(color: Colors.red, fontSize: 25),
+              ),
+            ],
+          )),
         ),
+      ));
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Container(
+        color: Colors.white,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return MaterialApp(
+        theme: ThemeData(accentColor: Color(COLOR_PRIMARY)),
+        debugShowCheckedModeBanner: false,
+        color: Color(COLOR_PRIMARY),
+        home: OnBoarding());
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (auth.FirebaseAuth.instance.currentUser != null && currentUser != null) {
+      if (state == AppLifecycleState.paused) {
+        //user offline
+        tokenStream.pause();
+        currentUser.active = false;
+        currentUser.lastOnlineTimestamp = Timestamp.now();
+        FireStoreUtils.updateCurrentUser(currentUser);
+      } else if (state == AppLifecycleState.resumed) {
+        //user online
+        tokenStream.resume();
+        currentUser.active = true;
+        FireStoreUtils.updateCurrentUser(currentUser);
+      }
+    }
+  }
+}
+
+class OnBoarding extends StatefulWidget {
+  @override
+  State createState() {
+    return OnBoardingState();
+  }
+}
+
+class OnBoardingState extends State<OnBoarding> {
+  Future hasFinishedOnBoarding() async {
+
+    // pushReplacement(context, new AuthScreen());
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool finishedOnBoarding = (prefs.getBool(FINISHED_ON_BOARDING) ?? false);
+
+    if (finishedOnBoarding) {
+      auth.User firebaseUser = auth.FirebaseAuth.instance.currentUser;
+      if (firebaseUser != null) {
+        User user = await FireStoreUtils().getCurrentUser(firebaseUser.uid);
+        if (user != null) {
+          MyAppState.currentUser = user;
+          pushReplacement(context, new HomeScreen(user: user));
+        } else {
+          pushReplacement(context, new AuthScreen());
+        }
+      } else {
+        pushReplacement(context, new AuthScreen());
+      }
+    } else {
+      pushReplacement(context, new OnBoardingScreen());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    hasFinishedOnBoarding();
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Color(COLOR_PRIMARY),
+  //     body: Center(
+  //       child: CircularProgressIndicator(
+  //         backgroundColor: Colors.black,
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Icon(
+              Icons.phone_iphone,
+              size: 150,
+              color: Color(COLOR_PRIMARY),
+            ),
+          ),
+          Padding(
+            padding:
+            const EdgeInsets.only(left: 16, top: 32, right: 16, bottom: 8),
+            child: Text(
+              'Say Hello To Your New App!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Color(COLOR_PRIMARY),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            child: Text(
+              'You\'ve just saved a week of development and headaches.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0, left: 40.0, top: 40),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: RaisedButton(
+                color: Color(COLOR_PRIMARY),
+                child: Text(
+                  'Log In',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                textColor: Colors.white,
+                splashColor: Color(COLOR_PRIMARY),
+                onPressed: () => push(context, new LoginScreen()),
+                padding: EdgeInsets.only(top: 12, bottom: 12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: Color(COLOR_PRIMARY))),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                right: 40.0, left: 40.0, top: 20, bottom: 20),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: FlatButton(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(COLOR_PRIMARY)),
+                ),
+                onPressed: () => push(context, new SignUpScreen()),
+                padding: EdgeInsets.only(top: 12, bottom: 12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: Color(COLOR_PRIMARY))),
+              ),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(new MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: new ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+//         // counter didn't reset back to zero; the application is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: new MyHomePage(title: 'Flutter Hello World'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+//
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => new _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: new Text(widget.title),
+//       ),
+//       body: new Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: new Column(
+//           // Column is also layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Invoke "debug paint" (press "p" in the console where you ran
+//           // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+//           // window in IntelliJ) to see the wireframe for each widget.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             new Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.display1,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: new FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: new Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
